6d8d1499bdd21a54aab03790df57a5f54eeb1194
Pritesh <priteshpmantri@gmail.com>
2022-04-12 20:59:59 +0530

    a and b are done

--- p2.c
+++ p2.c
@@ -24,10 +24,11 @@ void *th_function(void *args){
 
 	long long int local_sum = 0;
 	
-	for (int i = a->start; i < a->end; i++){
+	for (int i = a->start; i <= a->end; i++){
 		local_sum += numbers[i];
 	}
 
+	//add to the sum safely
 	pthread_mutex_lock(&lock);
 	sum += local_sum;
 	pthread_mutex_unlock(&lock);
@@ -47,14 +48,26 @@ int main() {
 
 	int num_per_thread = size/MAX_THREAD;
 
-	for (int i = 0; i < MAX_THREAD; i++){
+	pthread_t threads[MAX_THREAD];
+	struct args th_args[MAX_THREAD];
 
+	for (int i = 0; i < MAX_THREAD; i++){
+		struct args arg;
+		arg.start = i*num_per_thread + 1;
+		arg.end = (i+1)*num_per_thread;
+
+		th_args[i] = arg;
 	}
 
 	for (int i = 0; i < MAX_THREAD; i++){
-
+		pthread_create(&threads[i], NULL, th_function, &th_args[i]);
 	}
 
+	for (int i = 0; i < MAX_THREAD; i++) {
+		pthread_join(threads[i], NULL);
+	}
+
+	printf("sum is: %lld", sum);
 	pthread_mutex_destroy(&lock);
 	shmdt(numbers);
 
